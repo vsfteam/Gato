340a3f6ca21b00cd6f254f5b9f86df0641bb6549
yzhu <891085309@qq.com>
2020-09-16 01:24:37 +0800

    [Example] FIXME

--- examples/example_5.c
+++ examples/example_5.c
@@ -9,11 +9,14 @@ static void sample(surface_t *base, float fps)
 {
 	surface_clear(base, RGB(0xFFFFFF), 0, 0, base->width, base->height);
 	draw_image(base, "res/appstore.png", 0, 0, W, H);
+	//FIXME:
 	draw_circle(base, 150, 150, 120, (style_t){
-		fill_color : ARGB(0x2FFFFFFF),
+		fill_color : ARGB(0x5FFF0000),
 		border_radius : {1, 1, 1, 1},
-		stroke_color : ARGB(0x2FB2B2B2),
-		stroke_width : 2,
+		stroke_color : ARGB(0xFF00FF00),
+		stroke_width : 30,
+		n_shadow : 1,
+		shadow : (shadow_t[]){0, 0, 20, RGB(0)},
 	});
 }
 
